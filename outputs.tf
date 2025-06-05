adf85871f8fe75ed209cb2e5ff2a71e9a4308a3d
reddomeuk <academy@reddome.org>
2025-06-05 22:10:00 +0100

    rls08

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,10 @@
+# Resource Group Output
 output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = azurerm_resource_group.main.name  # Fixed to use created resource group
+  description = "Name of the created resource group"
+  value       = azurerm_resource_group.main.name
 }
 
+# Storage Account Outputs
 output "storage_account_name" {
   description = "Name of the storage account"
   value       = azurerm_storage_account.main.name
@@ -19,6 +21,7 @@ output "storage_account_primary_key" {
   sensitive   = true
 }
 
+# File Share Outputs
 output "file_share_name" {
   description = "Name of the file share"
   value       = azurerm_storage_share.attribute_management.name
@@ -29,6 +32,7 @@ output "file_share_url" {
   value       = azurerm_storage_share.attribute_management.url
 }
 
+# Automation Account Outputs
 output "automation_account_name" {
   description = "Name of the automation account"
   value       = azurerm_automation_account.main.name
@@ -40,30 +44,16 @@ output "automation_account_id" {
 }
 
 output "automation_managed_identity_principal_id" {
-  description = "Principal ID of the automation account's managed identity"
+  description = "Principal ID of the automation account managed identity"
   value       = azurerm_automation_account.main.identity[0].principal_id
 }
 
 output "automation_managed_identity_tenant_id" {
-  description = "Tenant ID of the automation account's managed identity"
+  description = "Tenant ID of the automation account managed identity"
   value       = azurerm_automation_account.main.identity[0].tenant_id
 }
 
-output "runbook_name" {
-  description = "Name of the PowerShell runbook"
-  value       = azurerm_automation_runbook.extension_attribute_management.name
-}
-
-output "web_app_name" {
-  description = "Name of the web application"
-  value       = azurerm_linux_web_app.main.name
-}
-
-output "web_app_url" {
-  description = "URL of the web application"
-  value       = "https://${azurerm_linux_web_app.main.default_hostname}"
-}
-
+# Key Vault Outputs
 output "key_vault_name" {
   description = "Name of the Key Vault"
   value       = azurerm_key_vault.main.name
@@ -74,80 +64,70 @@ output "key_vault_uri" {
   value       = azurerm_key_vault.main.vault_uri
 }
 
-output "html_interface_path" {
-  description = "Path to the HTML interface in the file share"
-  value       = "\\\\${azurerm_storage_account.main.name}.file.core.windows.net\\${azurerm_storage_share.attribute_management.name}\\config\\attribute-management.html"
+# Web App Outputs
+output "web_app_name" {
+  description = "Name of the web app"
+  value       = azurerm_linux_web_app.main.name
 }
 
+output "web_app_url" {
+  description = "URL of the web app"
+  value       = "https://${azurerm_linux_web_app.main.default_hostname}"
+}
+
+# Runbook Output
+output "runbook_name" {
+  description = "Name of the extension attribute management runbook"
+  value       = azurerm_automation_runbook.extension_attribute_management.name
+}
+
+# File Share Access Commands
 output "file_share_access_commands" {
-  description = "PowerShell commands to access the file share"
+  description = "Commands to access the file share"
   value = {
-    create_context = "New-AzStorageContext -StorageAccountName '${azurerm_storage_account.main.name}' -StorageAccountKey '[ACCESS_KEY]'"
-    list_files     = "Get-AzStorageFile -ShareName '${azurerm_storage_share.attribute_management.name}' -Context $ctx"
-    config_path    = "config/attribute-management.html"
-    logs_path      = "logs/"
-    reports_path   = "reports/"
+    azure_cli     = "az storage file list --account-name ${azurerm_storage_account.main.name} --share-name ${azurerm_storage_share.attribute_management.name}"
+    powershell    = "Get-AzStorageFile -ShareName ${azurerm_storage_share.attribute_management.name} -Context (New-AzStorageContext -StorageAccountName ${azurerm_storage_account.main.name} -StorageAccountKey '<key>')"
+    mount_windows = "net use Z: \\\\${azurerm_storage_account.main.name}.file.core.windows.net\\${azurerm_storage_share.attribute_management.name}"
+    mount_linux   = "sudo mount -t cifs //${azurerm_storage_account.main.name}.file.core.windows.net/${azurerm_storage_share.attribute_management.name} /mnt/azure-share -o username=${azurerm_storage_account.main.name},password=<key>"
   }
 }
 
-output "azure_ad_application" {
-  description = "Azure AD application details for Graph API access"
-  value = {
-    client_id    = azuread_application.extension_attribute_app.client_id  # Fixed deprecated attribute
-    object_id    = azuread_application.extension_attribute_app.object_id
-    display_name = azuread_application.extension_attribute_app.display_name
-  }
-}
-
+# Deployment Summary
 output "deployment_summary" {
-  description = "Summary of the deployed resources and next steps"
+  description = "Summary of deployed resources"
   value = {
-    resource_group     = azurerm_resource_group.main.name
-    storage_account    = azurerm_storage_account.main.name
-    file_share         = azurerm_storage_share.attribute_management.name
+    resource_group    = azurerm_resource_group.main.name
+    location         = azurerm_resource_group.main.location
+    storage_account  = azurerm_storage_account.main.name
     automation_account = azurerm_automation_account.main.name
-    runbook           = azurerm_automation_runbook.extension_attribute_management.name
-    web_app           = azurerm_linux_web_app.main.name
-    web_app_url       = "https://${azurerm_linux_web_app.main.default_hostname}"
-    key_vault         = azurerm_key_vault.main.name
-    html_interface    = "\\\\${azurerm_storage_account.main.name}.file.core.windows.net\\${azurerm_storage_share.attribute_management.name}\\config\\attribute-management.html"
-    next_steps = [
-      "1. Access the web interface at: https://${azurerm_linux_web_app.main.default_hostname}",
-      "2. Deploy the web application code to the App Service",
-      "3. Configure email settings in Key Vault (email-from, email-to)",
-      "4. Test extension attribute management in What-If mode",
-      "5. Configure Graph API admin consent if needed",
-      "6. Set up monitoring and alerts as required"
-    ]
+    key_vault       = azurerm_key_vault.main.name
+    web_app         = azurerm_linux_web_app.main.name
+    runbook         = azurerm_automation_runbook.extension_attribute_management.name
+    deployment_time = timestamp()
   }
 }
 
-output "graph_api_permissions" {
-  description = "Microsoft Graph API permissions assigned"
-  value = [
-    "User.Read.All",
-    "User.ReadWrite.All", 
-    "Directory.ReadWrite.All",
-    "Mail.Send"
-  ]
-}
-
-output "role_assignments" {
-  description = "Role assignments for the automation account managed identity"
-  value = [
-    "Storage File Data SMB Share Contributor",
-    "Storage Account Contributor"
-  ]
-}
-
-output "monitoring_and_logs" {
-  description = "Locations for monitoring and logs"
+# Manual Configuration Required
+output "manual_steps_required" {
+  description = "Manual steps required after deployment"
   value = {
-    execution_logs     = "\\\\${azurerm_storage_account.main.name}.file.core.windows.net\\${azurerm_storage_share.attribute_management.name}\\logs\\"
-    html_reports      = "\\\\${azurerm_storage_account.main.name}.file.core.windows.net\\${azurerm_storage_share.attribute_management.name}\\reports\\"
-    config_backups    = "\\\\${azurerm_storage_account.main.name}.file.core.windows.net\\${azurerm_storage_share.attribute_management.name}\\backups\\"
-    automation_logs   = "Azure Portal -> Automation Account -> Jobs"
-    application_insights = azurerm_application_insights.main.name
-    web_app_logs      = "Azure Portal -> App Service -> Log stream"
+    azure_ad_app = "Create Azure AD application manually in Azure Portal with Graph API permissions"
+    role_assignments = "Assign Storage Blob Data Contributor and Automation Contributor roles manually"
+    automation_modules = "Install PowerShell modules manually: Microsoft.Graph.Authentication, Microsoft.Graph.Users, etc."
+    graph_permissions = "Grant admin consent for Graph API permissions in Azure AD"
+    web_app_code = "Deploy web application code to the App Service"
   }
+}
+
+# Next Steps
+output "next_steps" {
+  description = "Next steps after basic deployment"
+  value = [
+    "1. Create Azure AD Application with Graph API permissions",
+    "2. Assign required RBAC roles to managed identities",
+    "3. Install PowerShell modules in Automation Account",
+    "4. Deploy web application code",
+    "5. Test runbook execution",
+    "6. Configure email notifications in Key Vault"
+  ]
 }
